3a7eafcc3360f18b8fe23156a8cc3b42017f2336
prezrenz <perezrenjie20@gmail.com>
2024-02-16 04:22:49 +0800

    set orb random angle to -5 to 5

--- OrbSpawner.gd
+++ OrbSpawner.gd
@@ -8,9 +8,9 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$Orb.rotation_degrees += randi() % 5 - 5
-	$Orb2.rotation_degrees += randi() % 5 - 5
-	$Orb3.rotation_degrees += randi() % 5 - 5
+	$Orb.rotation_degrees += randi() % -5 - 5
+	$Orb2.rotation_degrees += randi() % -5 - 5
+	$Orb3.rotation_degrees += randi() % -5 - 5
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
