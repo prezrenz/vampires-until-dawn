936dbb136ce1fd71e003eb32d6ff7acee53d7234
prezrenz <perezrenjie20@gmail.com>
2024-02-16 10:20:28 +0800

    remove orb spawner debug

--- Game.gd
+++ Game.gd
@@ -4,7 +4,7 @@ extends Node2D
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-var orb = preload("res://OrbSpawner.tscn")
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -18,6 +18,4 @@ func _ready():
 
 func _on_StartWave_timeout():
 	# Replace with star wave function
-	var new_orb = orb.instance()
-	new_orb.global_transform = $VampireSpawn.global_transform
-	add_child(new_orb)
+	pass
